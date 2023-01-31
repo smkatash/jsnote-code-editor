88eaff44d35f3c5b1a4b41a94a59e07a73b30ddb
smkatash <tashbaevakanykei@gmail.com>
2023-01-31 17:18:33 +0100

    update

--- src/state/reducers/cell-reducer.ts
+++ src/state/reducers/cell-reducer.ts
@@ -1,6 +1,8 @@
 import { ActionType } from "../action-types";
 import { Action } from "../actions";
 import { Cell } from "../cell"
+import produce from "immer";
+import { stat } from "fs";
 
 interface CellState {
 	loading: boolean;
@@ -18,28 +20,47 @@ const initialState: CellState = {
 	data: {}
 };
 
-const reducer = (state: CellState = initialState, action: Action): CellState => {
+const reducer = produce((state: CellState = initialState, action: Action) => {
 	switch(action.type) {
 		case ActionType.UPDATE_CELL:
 			const { id, content } = action.payload;
-			return {
-				...state,
-				data: {
-					...state.data, 
-					[id]: {...state.data[id], 
-					content
-					},
-				},
-			};
+			state.data[id].content = content;
+			return state;
 		case ActionType.MOVE_CELL:
+			const { direction } = action.payload;
+			const index = state.order.findIndex((id) => id === action.payload.id );
+			const targetIndex = direction === 'up' ? index - 1 : index + 1;
+			if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+				return state;
+			}
+			state.order[index] = state.order[targetIndex];
+			state.order[targetIndex] = action.payload.id;
 			return state;
 		case ActionType.INSERT_CELL_BEFORE:
+			const cell: Cell = {
+				content: '',
+				type: action.payload.type,
+				id: randomId()
+			};
+			state.data[cell.id] = cell;
+			const foundindex = state.order.findIndex(id => id === action.payload.id);
+			if (foundindex < 0) {
+				state.order.push(cell.id);
+			} else {
+				state.order.splice(foundindex, 0, cell.id);
+			}
 			return state;
 		case ActionType.DELETE_CELL:
+			delete state.data[action.payload];
+			state.order = state.order.filter( id => id !== action.payload);
 			return state;
 		default:
 			return state;
 	}
+});
+
+const randomId = () => {
+	return Math.random().toString(36).substring(2,5);
 };
 
 export default reducer;
