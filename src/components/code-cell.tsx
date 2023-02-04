3a55662bb020e591ddebb659ff9699596ebd5c63
smkatash <tashbaevakanykei@gmail.com>
2023-02-04 16:53:14 +0100

    bundling update

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -18,7 +18,27 @@ const CodeCell: React.FC<CodeCellProps> = ({cell}) => {
 	const cumulativeCode = useTypedSelector((state) => {
 		const { data, order } = state.cells;
 		const orderedCells = order.map(id => data[id]);
-		const cumulativeCode = [];
+		const cumulativeCode = [
+			`
+				import _React from 'react';
+				import _ReactDOM from 'react-dom';
+				const show = (value) => {
+					const root = document.querySelector('#root');
+					
+					if (typeof value === 'object') {
+						if (value.$$typeof && value.props) {
+							_ReactDOM.render(value, root);
+						} else {
+							document.querySelector('#root').innerHTML = JSON.stringify(value);
+						}
+					} else {
+						document.querySelector('#root').innerHTML = value;
+					}
+				};
+			`
+		];
+
+
 		for (let c of orderedCells) {
 			if (c.type === 'code') {
 				cumulativeCode.push(c.content);
